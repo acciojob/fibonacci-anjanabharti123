11d51887d6bb354110e061d313a1dab61b9d453b
Anjana Bharti <143540468+anjanabharti123@users.noreply.github.com>
2024-08-01 16:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function fibonacci(num) {
 // your code here
-	let a = 0, b = 1, c, i;
-	if(num == 0)
-		return a;
-	for(i = 2; i <= num; i++){
-		c = a+b;
-		a = b;
-		b = c;
-	}
-	return b;
+	if (num === 0) return 0;
+
+  // Initialize the first two Fibonacci numbers
+  let a = 0, b = 1, c;
+
+  // Iterate to compute Fibonacci numbers up to num
+  for (let i = 2; i <= num; i++) {
+    c = a + b; // Compute the next Fibonacci number
+    a = b;     // Update a to the previous Fibonacci number
+    b = c;     // Update b to the new Fibonacci number
+  }
+
+  // Return the nth Fibonacci number
+  return b;
 }
 
 module.exports = fibonacci;
